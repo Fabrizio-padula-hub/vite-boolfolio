bf8a8cf4ec4719ca996f998bf9e895b3118be16f
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-06-10 18:59:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default{
+
+}
+</script>
+
+
+<template>
+    <h1>prova</h1>
+</template>
+
+
+<style lang="scss">
+@use './style/generic';
+@use './style/partials/variables' as *;
+</style>
