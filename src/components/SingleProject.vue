01ba8c88b82b3bf631849af9f90e571824d821b3
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-06-11 17:04:08 +0200

    feat: print type and technologies, and change color text.

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -3,6 +3,15 @@ export default{
     name: 'SingleProject',
     props: {
         projectInfo: Object
+    },
+    methods:{
+        truncateText(text){
+            if(text.length >100){
+                return text.substr(0, 99) + '...';
+            }
+
+            return text;
+        }
     }
 }
 </script>
@@ -10,10 +19,18 @@ export default{
 <template>
 
     <div class="card my-3">
-        <img src="..." class="card-img-top" :alt="projectInfo.name">
         <div class="card-body">
             <h5 class="card-title">{{ projectInfo.name }}</h5>
-            <p v-if="projectInfo.summary" class="card-text">{{ projectInfo.summary }}</p>
+            <div v-if="projectInfo.type">
+                <strong>tipo</strong>: {{ projectInfo.type.name }}
+            </div>
+            <div v-if="projectInfo.technologies.length > 0">
+                <strong>Tecnologie</strong>:
+                <span v-for="tech in projectInfo.technologies">
+                    {{ tech.name }}
+                &nbsp</span>
+            </div>
+            <p v-if="projectInfo.summary" class="card-text mt-4">{{ truncateText(projectInfo.summary) }}</p>
             <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
         </div>
     </div>
